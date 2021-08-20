8ab25aa6c11b7df7150ed3f80a88fa5de3889909
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2021-08-20 17:46:43 -0400

    Implemented research header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import Hero from "./components/Hero";
+import { DarkHeader } from "./components/Headers";
+
 import "./styles/styles.scss";
 
 function App() {
   return (
     <div className="full-container">
       <Hero></Hero>
+      <DarkHeader text="I. Research"></DarkHeader>
     </div>
   );
 }
