c4bf622d0d2a84ffc0ea6f7f5cff74adb5cabb95
Joseph Chen <jchen42703@gmail.com>
2022-11-06 05:49:31 -0500

    Better About Me description

--- v2/src/home/AboutMe.tsx
+++ v2/src/home/AboutMe.tsx
@@ -22,7 +22,34 @@ function AboutMe() {
 				>
 					here
 				</a>
-				!
+				! Quick caveat: it excludes 3+ years of research experience
+				prior to college to be concise, but that experience is on my
+				LinkedIn ¯\_(ツ)_/¯.
+			</p>
+			<br />
+			<p>
+				A little about me: I love to code in my free-time, whether that
+				be implementing new business ideas or filling in my knowledge
+				gaps! I started seriously coding in my sophomore of high school
+				to do independent original machine learning research and won
+				some awards at state competitions. I eventually got sick of the
+				never-ending parameter tuning grind and horrendous
+				reproducibility issues in ML research after 3 years. Ever since
+				then, I've been dabbling in full-stack, desktop application, and
+				CLI development. I've even been fortunate enough to be the
+				founding SWE #2 of a small, successful startup!
+			</p>
+			<br />
+			<p>
+				Besides coding, I cook, work out and dance in my free time. I
+				compiled some tasy and easy recipes @&nbsp;
+				<a
+					href="https://github.com/jchen42703/broke-college-student-recipes"
+					className="font-bold underline"
+				>
+					https://github.com/jchen42703/broke-college-student-recipes
+				</a>
+				. Feel free to try them out and let me know what you think!
 			</p>
 		</div>
 	);
