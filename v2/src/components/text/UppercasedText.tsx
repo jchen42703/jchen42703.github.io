a4f621bc60ad112167d9c3cc79c18a4f5d32bb9a
Joseph Chen <jchen42703@gmail.com>
2022-11-06 16:29:42 -0500

    smooth scrolling for UppercasedText with href

--- v2/src/components/text/UppercasedText.tsx
+++ v2/src/components/text/UppercasedText.tsx
@@ -1,3 +1,8 @@
+/**
+ * Abstracts uppercased text, whether that be headers or links.
+ *
+ * For links, it implements a smooth scroll to the href if applicable.
+ */
 function UppercasedText({
 	text,
 	href,
@@ -25,8 +30,25 @@ function UppercasedText({
 	}
 
 	if (href) {
+		let onClick: React.MouseEventHandler<HTMLAnchorElement> | undefined =
+			undefined;
+		// Smoothly scroll to the div with anchor element (<a></a>)
+		if (href.startsWith("#")) {
+			onClick = (e) => {
+				e.preventDefault();
+				const anchorElem = document.querySelector(href);
+				if (!anchorElem) {
+					console.error(`anchor element for ${href} not found`);
+					return;
+				}
+
+				anchorElem.scrollIntoView({
+					behavior: "smooth",
+				});
+			};
+		}
 		return (
-			<a className={className} href={href}>
+			<a className={className} href={href} onClick={onClick}>
 				{text}
 			</a>
 		);
