b97a881dff5a2c37e39559de88b65656b38ea503
Joseph Chen <jchen42703@gmail.com>
2022-11-06 04:55:53 -0500

    Basic welcome page

---
+++ v2/src/home/Welcome.tsx
@@ -0,0 +1,113 @@
+function UppercasedText({
+	text,
+	href,
+	underline = false,
+	bold = false,
+	classes = "",
+}: {
+	text: string;
+	href?: string;
+	underline?: boolean;
+	bold?: boolean;
+	classes?: string;
+}) {
+	let className = "uppercase";
+	if (underline) {
+		className = className + " underline";
+	}
+
+	if (bold) {
+		className = className + " font-bold";
+	}
+
+	if (classes) {
+		className = className + " " + classes;
+	}
+
+	if (href) {
+		return (
+			<a className={className} href={href}>
+				{text}
+			</a>
+		);
+	}
+	return <span className={className}>{text}</span>;
+}
+
+/**
+ * Welcome and Navigation Component
+ */
+function Welcome() {
+	const noLinkFields = ["Joseph Chen", "NYC, NY", "CS 3rd Year @ CWRU"];
+
+	const regLinkFields = [
+		{
+			title: "<About Me />",
+			href: "#aboutme",
+		},
+		{
+			title: "<Contact Me />",
+			href: "#contact",
+		},
+	];
+
+	const workFields = [
+		{
+			title: "<Blockchain />",
+			href: "#blockchain",
+		},
+		{
+			title: "<Automation & Reverse Engineering />",
+			href: "#automation",
+		},
+		{
+			title: "<Machine Learning />",
+			href: "#machinelearning",
+		},
+	];
+
+	return (
+		<div className="grid grid-cols-4 welcome-container gap-x-0.5">
+			<div className="col-span-1 flex flex-col">
+				{noLinkFields.map((text) => (
+					<UppercasedText text={text} bold={true}></UppercasedText>
+				))}
+			</div>
+
+			<div className="col-span-1 flex flex-col">
+				<UppercasedText text={"Menu"} bold={true}></UppercasedText>
+				<br></br>
+				{regLinkFields.map(({ title, href }) => (
+					<UppercasedText
+						text={title}
+						href={href}
+						bold={true}
+					></UppercasedText>
+				))}
+				<br></br>
+
+				<UppercasedText text={"My Work"} bold={true}></UppercasedText>
+				<br></br>
+				{workFields.map(({ title, href }) => (
+					<UppercasedText
+						text={title}
+						href={href}
+						bold={true}
+					></UppercasedText>
+				))}
+			</div>
+
+			<div className="col-span-2 flex flex-col text-right">
+				{["Undergrad", "&", "(Ex) Founding SWE"].map((text) => (
+					<UppercasedText
+						text={text}
+						bold={true}
+						classes={"text-6xl"}
+					></UppercasedText>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default Welcome;
