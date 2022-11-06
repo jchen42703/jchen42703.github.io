4e393a2c3341014c2209443b729ff10982bfadc8
Joseph Chen <jchen42703@gmail.com>
2022-11-06 05:00:51 -0500

    Moved UppercasedText to its separate file

--- v2/src/home/Welcome.tsx
+++ v2/src/home/Welcome.tsx
@@ -1,38 +1,4 @@
-function UppercasedText({
-	text,
-	href,
-	underline = false,
-	bold = false,
-	classes = "",
-}: {
-	text: string;
-	href?: string;
-	underline?: boolean;
-	bold?: boolean;
-	classes?: string;
-}) {
-	let className = "uppercase";
-	if (underline) {
-		className = className + " underline";
-	}
-
-	if (bold) {
-		className = className + " font-bold";
-	}
-
-	if (classes) {
-		className = className + " " + classes;
-	}
-
-	if (href) {
-		return (
-			<a className={className} href={href}>
-				{text}
-			</a>
-		);
-	}
-	return <span className={className}>{text}</span>;
-}
+import UppercasedText from "../components/text/UppercasedText";
 
 /**
  * Welcome and Navigation Component
