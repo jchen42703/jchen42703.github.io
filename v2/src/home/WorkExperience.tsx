383d91caf40d5c37f3300ab942ed2931582a6f58
Joseph Chen <jchen42703@gmail.com>
2022-11-07 14:35:51 -0500

    Made my work experience slide into view

--- v2/src/home/WorkExperience.tsx
+++ v2/src/home/WorkExperience.tsx
@@ -2,11 +2,24 @@ import UppercasedText from "../components/text/UppercasedText";
 import NemesisExperience from "./work/nemesis";
 import Parallaxis2020Experience from "./work/parallaxis2020";
 import Parallaxis2021Experience from "./work/parallaxis2021";
+import { motion } from "framer-motion";
 
 /**
  * Work Experience Component
  */
 function WorkExperience() {
+	const slideIntoViewAnimation = {
+		initial: { x: "-100%", opacity: 0 },
+		whileInView: { x: 0, opacity: 1 },
+		viewport: {
+			once: true,
+		},
+		transition: {
+			ease: "anticipate",
+			duration: 1.25,
+		},
+	};
+
 	return (
 		<div id="aboutme" className="content-container-no-top">
 			<UppercasedText
@@ -15,9 +28,15 @@ function WorkExperience() {
 				classes={"text-3xl"}
 			></UppercasedText>
 			<br />
-			<NemesisExperience></NemesisExperience>
-			<Parallaxis2021Experience></Parallaxis2021Experience>
-			<Parallaxis2020Experience></Parallaxis2020Experience>
+			<motion.div {...slideIntoViewAnimation}>
+				<NemesisExperience></NemesisExperience>
+			</motion.div>
+			<motion.div {...slideIntoViewAnimation}>
+				<Parallaxis2021Experience></Parallaxis2021Experience>
+			</motion.div>
+			<motion.div {...slideIntoViewAnimation}>
+				<Parallaxis2020Experience></Parallaxis2020Experience>
+			</motion.div>
 		</div>
 	);
 }
