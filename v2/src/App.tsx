b0624993ddc891c94c8738cb882f103749858962
Joseph Chen <jchen42703@gmail.com>
2022-11-07 18:52:12 -0500

    Added a scroll progress bar

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -5,12 +5,19 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import "./styles/styles.scss";
 import ScrollButton from "./components/button/ScrollToTopButton";
+import { motion, useScroll } from "framer-motion";
 
 library.add(fab);
 
 function App() {
+	const { scrollYProgress } = useScroll();
+
 	return (
 		<>
+			<motion.div
+				className="progress-bar"
+				style={{ scaleX: scrollYProgress }}
+			/>
 			<Welcome></Welcome>
 			<AboutMe></AboutMe>
 			<WorkExperience></WorkExperience>
