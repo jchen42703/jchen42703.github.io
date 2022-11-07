b91605f9bec7a853b39d04bf4307dead4243a96e
Joseph Chen <jchen42703@gmail.com>
2022-11-07 16:30:26 -0500

    Animate the scroll to top button being visible

--- v2/src/components/button/ScrollToTopButton.tsx
+++ v2/src/components/button/ScrollToTopButton.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { BsCaretUpSquareFill } from "react-icons/bs";
 
@@ -25,12 +26,23 @@ const ScrollButton = () => {
 	window.addEventListener("scroll", toggleVisible);
 
 	return (
-		<div className="scroll-to-top-btn">
-			<BsCaretUpSquareFill
-				onClick={scrollToTop}
-				style={{ display: visible ? "inline" : "none" }}
-			/>
-		</div>
+		<AnimatePresence>
+			{visible && (
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ ease: "easeOut", duration: 0.5 }}
+				>
+					<div className="scroll-to-top-btn">
+						<BsCaretUpSquareFill
+							onClick={scrollToTop}
+							// style={{ display: visible ? "inline" : "none" }}
+						/>
+					</div>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	);
 };
 
