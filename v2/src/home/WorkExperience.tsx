e2548ca8cda27e1f159db01882796762e51b789d
Joseph Chen <jchen42703@gmail.com>
2022-11-07 05:37:09 -0500

    Renamed work section to Work & Research

--- v2/src/home/WorkExperience.tsx
+++ v2/src/home/WorkExperience.tsx
@@ -10,7 +10,7 @@ function WorkExperience() {
 	return (
 		<div id="aboutme" className="content-container-no-top">
 			<UppercasedText
-				text={"Work / Research / Projects"}
+				text={"Work & Research"}
 				bold={true}
 				classes={"text-3xl"}
 			></UppercasedText>
