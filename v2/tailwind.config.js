6cdc29fe7146c9d680b68afed5a2a0c3ae39863f
Joseph Chen <jchen42703@gmail.com>
2022-11-08 00:35:36 -0500

    Set 885px as the laptop breakpoint

--- v2/tailwind.config.js
+++ v2/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
 	content: ["./src/**/*.{ts,tsx}"],
 	theme: {
 		extend: {},
+		screens: {
+			tablet: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			laptop: "885px",
+			// => @media (min-width: 1024px) { ... }
+
+			desktop: "1280px",
+			// => @media (min-width: 1280px) { ... }
+		},
 	},
 	plugins: [],
 };
