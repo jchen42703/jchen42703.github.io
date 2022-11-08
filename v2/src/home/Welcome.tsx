45bbb1f88a4f3b0a622149dca8ecf47ee4a64b0b
Joseph Chen <jchen42703@gmail.com>
2022-11-08 10:54:58 -0500

    Staggered animated the welcome text

--- v2/src/home/Welcome.tsx
+++ v2/src/home/Welcome.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useMediaQuery } from "react-responsive";
 import UppercasedText from "../components/text/UppercasedText";
 import { mediaQueries } from "../services/mediaQueries";
@@ -12,6 +13,32 @@ function Welcome() {
 	});
 
 	const noLinkFields = ["Joseph Chen", "NYC, NY", "CS 3rd Year @ CWRU"];
+
+	const variants = {
+		hidden: { y: "-100%" },
+		show: {
+			y: 0,
+			transition: {
+				staggerChildren: 0.5,
+				delayChildren: 0.3,
+			},
+		},
+	};
+
+	const textVariants = [
+		{
+			hidden: { y: "-100%" },
+			show: { y: 0 },
+		},
+		{
+			hidden: { y: "-400%" },
+			show: { y: 0 },
+		},
+		{
+			hidden: { x: "200%" },
+			show: { x: 0 },
+		},
+	];
 
 	// Responsively renders the top components (basic info + nav menu)
 	const renderTop = () => {
@@ -49,20 +76,27 @@ function Welcome() {
 			</>
 		);
 	};
+
 	return (
 		<div className="flex flex-col md:grid md:grid-cols-4 content-container gap-x-0.75">
 			{renderTop()}
 
-			<div className="flex flex-col text-left mt-4 md:mt-0 md:col-span-2 md:text-right">
-				{["Undergrad", "&", "(Ex) Founding SWE"].map((text) => (
-					<UppercasedText
-						key={text}
-						text={text}
-						bold={true}
-						classes="text-7xl"
-					></UppercasedText>
+			<motion.div
+				className="flex flex-col text-left mt-4 md:mt-0 md:col-span-2 md:text-right"
+				variants={variants}
+				initial="hidden"
+				animate="show"
+			>
+				{["Undergrad", "&", "(Ex) Founding SWE"].map((text, i) => (
+					<motion.div key={text} variants={textVariants[i]}>
+						<UppercasedText
+							text={text}
+							bold={true}
+							classes="text-7xl"
+						></UppercasedText>
+					</motion.div>
 				))}
-			</div>
+			</motion.div>
 		</div>
 	);
 }
