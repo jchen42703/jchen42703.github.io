653fdab243cd29fefc618597e148d99fe60d4096
Joseph Chen <jchen42703@gmail.com>
2022-11-08 01:26:53 -0500

    better names for responsive breakpoints

--- v2/tailwind.config.js
+++ v2/tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
 	theme: {
 		extend: {},
 		screens: {
-			tablet: "640px",
+			sm: "640px",
 			// => @media (min-width: 640px) { ... }
 
-			laptop: "885px",
+			md: "885px",
 			// => @media (min-width: 1024px) { ... }
 
-			desktop: "1280px",
+			lg: "1280px",
 			// => @media (min-width: 1280px) { ... }
 		},
 	},
