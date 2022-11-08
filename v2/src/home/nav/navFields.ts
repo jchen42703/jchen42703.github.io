35fd94763e11e5cb805d26cca33c3de11cf419f8
Joseph Chen <jchen42703@gmail.com>
2022-11-08 02:42:25 -0500

    Moved the navFields to constants for reusability

---
+++ v2/src/home/nav/navFields.ts
@@ -0,0 +1,37 @@
+export const regLinkFields = [
+	{
+		title: "<About Me />",
+		href: "#aboutme",
+	},
+	{
+		title: "<Resume />",
+		href: "https://github.com/jchen42703/resumes/blob/main/latest_generic.pdf",
+	},
+	{
+		title: "<Contact Me />",
+		href: "#contact",
+	},
+];
+
+export const workFields = [
+	{
+		title: "<Nemesis />",
+		href: "#nemesis",
+	},
+	{
+		title: "<Parallaxis (SWE) />",
+		href: "#parallaxis-swe",
+	},
+	{
+		title: "<Parallaxis (ML) />",
+		href: "#parallaxis-ml",
+	},
+	{
+		title: "<Machine Learning Research />",
+		href: "#machinelearning",
+	},
+	{
+		title: "<Applied ML (Hobby) />",
+		href: "#projects-mlops",
+	},
+];
