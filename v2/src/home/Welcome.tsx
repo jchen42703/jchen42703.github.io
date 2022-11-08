b44e4696a37edee5ffc756565b51f4bb3e927fca
Joseph Chen <jchen42703@gmail.com>
2022-11-08 01:50:49 -0500

    Made the top components responsive (big text goes below menu)

--- v2/src/home/Welcome.tsx
+++ v2/src/home/Welcome.tsx
@@ -1,28 +1,60 @@
+import { useMediaQuery } from "react-responsive";
 import UppercasedText from "../components/text/UppercasedText";
-import NavigationMenu from "./NavigationMenu";
+import { mediaQueries } from "../services/mediaQueries";
+import NavigationMenu from "./nav/NavigationMenu";
 
 /**
  * Welcome and Navigation Component
  */
 function Welcome() {
+	const isMobile = useMediaQuery({
+		query: mediaQueries.limitToMobile,
+	});
+
 	const noLinkFields = ["Joseph Chen", "NYC, NY", "CS 3rd Year @ CWRU"];
 
+	// Responsively renders the top components (basic info + nav menu)
+	const renderTop = () => {
+		console.log("is mobile: ", isMobile);
+		if (isMobile) {
+			return (
+				<div className="flex flex-row justify-between">
+					<div className="flex flex-col">
+						{noLinkFields.map((text) => (
+							<UppercasedText
+								key={text}
+								text={text}
+								bold={true}
+								classes="text-lg"
+							></UppercasedText>
+						))}
+					</div>
+					<NavigationMenu isMobile={isMobile}></NavigationMenu>
+				</div>
+			);
+		}
+
+		return (
+			<>
+				<div className="col-span-1 flex flex-col">
+					{noLinkFields.map((text) => (
+						<UppercasedText
+							key={text}
+							text={text}
+							bold={true}
+							classes="text-lg"
+						></UppercasedText>
+					))}
+				</div>
+				<NavigationMenu isMobile={isMobile}></NavigationMenu>
+			</>
+		);
+	};
 	return (
-		<div className="grid grid-cols-4 content-container gap-x-0.75">
-			<div className="col-span-1 flex flex-col">
-				{noLinkFields.map((text) => (
-					<UppercasedText
-						key={text}
-						text={text}
-						bold={true}
-						classes="text-lg"
-					></UppercasedText>
-				))}
-			</div>
+		<div className="flex flex-col md:grid md:grid-cols-4 content-container gap-x-0.75">
+			{renderTop()}
 
-			<NavigationMenu></NavigationMenu>
-
-			<div className="col-span-2 flex flex-col text-right">
+			<div className="flex flex-col text-left md:col-span-2 md:text-right">
 				{["Undergrad", "&", "(Ex) Founding SWE"].map((text) => (
 					<UppercasedText
 						key={text}
