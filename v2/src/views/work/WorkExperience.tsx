23c94b319a41ad74a19a685be06aa553be05ea3f
Joseph Chen <jchen42703@gmail.com>
2022-11-09 18:58:26 -0500

    Added a proper welcome for /work

--- v2/src/views/work/WorkExperience.tsx
+++ v2/src/views/work/WorkExperience.tsx
@@ -1,29 +1,58 @@
+import { motion, Variants } from "framer-motion";
 import UppercasedText from "../../components/text/UppercasedText";
 import NemesisExperience from "../work/nemesis";
 import Parallaxis2020Experience from "../work/parallaxis2020";
 import Parallaxis2021Experience from "../work/parallaxis2021";
 import SlideIntoViewAnimation from "../../components/animations/SlideIntoView";
+import Welcome from "../../components/Welcome";
 
 /**
  * Work Experience Component
  */
 function WorkExperience() {
+	const textVariants: Variants[] = [
+		{
+			hidden: { x: "200%" },
+			show: { x: 0 },
+		},
+	];
+
 	return (
-		<div id="workexp" className="content-container-no-top">
-			<UppercasedText
-				text={"Work & Research"}
-				bold={true}
-				classes={"text-3xl"}
-			></UppercasedText>
-			<SlideIntoViewAnimation>
-				<NemesisExperience></NemesisExperience>
-			</SlideIntoViewAnimation>
-			<SlideIntoViewAnimation>
-				<Parallaxis2021Experience></Parallaxis2021Experience>
-			</SlideIntoViewAnimation>
-			<SlideIntoViewAnimation>
-				<Parallaxis2020Experience></Parallaxis2020Experience>
-			</SlideIntoViewAnimation>
+		<div id="workexp">
+			<Welcome>
+				{["Work Experience"].map((text, i) => (
+					<motion.div
+						key={text}
+						variants={textVariants[i]}
+						transition={{
+							ease: "anticipate",
+							duration: 1.5,
+						}}
+					>
+						<UppercasedText
+							text={text}
+							bold={true}
+							classes="text-7xl"
+						></UppercasedText>
+					</motion.div>
+				))}
+			</Welcome>
+			<div className="content-container-no-top">
+				<UppercasedText
+					text={"Work & Research"}
+					bold={true}
+					classes={"text-3xl"}
+				></UppercasedText>
+				<SlideIntoViewAnimation>
+					<NemesisExperience></NemesisExperience>
+				</SlideIntoViewAnimation>
+				<SlideIntoViewAnimation>
+					<Parallaxis2021Experience></Parallaxis2021Experience>
+				</SlideIntoViewAnimation>
+				<SlideIntoViewAnimation>
+					<Parallaxis2020Experience></Parallaxis2020Experience>
+				</SlideIntoViewAnimation>
+			</div>
 		</div>
 	);
 }
