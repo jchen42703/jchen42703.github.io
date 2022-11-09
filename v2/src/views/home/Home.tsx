c8b3af913cb1eb350b21dad0c008c2d095819496
Joseph Chen <jchen42703@gmail.com>
2022-11-09 18:46:52 -0500

    Integrated Welcome changes to Home

--- v2/src/views/home/Home.tsx
+++ v2/src/views/home/Home.tsx
@@ -1,10 +1,48 @@
 import Welcome from "../../components/Welcome";
 import AboutMe from "./AboutMe";
+import { motion } from "framer-motion";
+import UppercasedText from "../../components/text/UppercasedText";
 
 function Home() {
+	const variants = {
+		hidden: { y: "-100%" },
+		show: {
+			y: 0,
+			transition: {
+				staggerChildren: 0.5,
+				delayChildren: 0.3,
+			},
+		},
+	};
+
+	const textVariants = [
+		{
+			hidden: { y: "-100%" },
+			show: { y: 0 },
+		},
+		{
+			hidden: { y: "-550%" },
+			show: { y: 0 },
+		},
+		{
+			hidden: { x: "200%" },
+			show: { x: 0 },
+		},
+	];
+
 	return (
 		<>
-			<Welcome></Welcome>
+			<Welcome variants={variants}>
+				{["Undergrad", "&", "(Ex) Founding SWE"].map((text, i) => (
+					<motion.div key={text} variants={textVariants[i]}>
+						<UppercasedText
+							text={text}
+							bold={true}
+							classes="text-7xl"
+						></UppercasedText>
+					</motion.div>
+				))}
+			</Welcome>
 			<AboutMe></AboutMe>
 		</>
 	);
