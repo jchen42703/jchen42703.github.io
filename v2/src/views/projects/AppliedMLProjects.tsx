362b21e7e15dbb9f48308484a784a94cff6092bd
Joseph Chen <jchen42703@gmail.com>
2022-12-19 21:27:14 -0500

    feat: AppledMLProjects component structure setup

--- v2/src/views/projects/AppliedMLProjects.tsx
+++ v2/src/views/projects/AppliedMLProjects.tsx
@@ -2,7 +2,8 @@ import { motion, Variants } from "framer-motion";
 import SlideIntoViewAnimation from "../../components/animations/SlideIntoView";
 import UppercasedText from "../../components/text/UppercasedText";
 import Welcome from "../../components/Welcome";
-import FullStackProjects from "./webDev/LyneProject";
+import EasierMTLProject from "./appliedML/EasierMTLProject";
+import IntroAppliedML from "./appliedML/IntroAppliedML";
 
 /**
  * AppliedMLProjects Component
@@ -35,9 +36,17 @@ function AppliedMLProjects() {
 					</motion.div>
 				))}
 			</Welcome>
+			<SlideIntoViewAnimation>
+				<IntroAppliedML></IntroAppliedML>
+			</SlideIntoViewAnimation>
 			<div className="content-container-no-top">
 				<SlideIntoViewAnimation>
-					<FullStackProjects></FullStackProjects>
+					<UppercasedText
+						text={"Timeline"}
+						bold={true}
+						classes={"text-3xl"}
+					></UppercasedText>
+					<EasierMTLProject></EasierMTLProject>
 				</SlideIntoViewAnimation>
 			</div>
 		</div>
