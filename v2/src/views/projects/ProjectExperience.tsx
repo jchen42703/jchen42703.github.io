0c636c0506b5c471191e6ba88120a6b57f9ac8dd
Joseph Chen <jchen42703@gmail.com>
2022-12-19 20:57:39 -0500

    feat: proper /projects page

--- v2/src/views/projects/ProjectExperience.tsx
+++ v2/src/views/projects/ProjectExperience.tsx
@@ -1,8 +1,7 @@
 import { motion, Variants } from "framer-motion";
-import SlideIntoViewAnimation from "../../components/animations/SlideIntoView";
 import UppercasedText from "../../components/text/UppercasedText";
+import UnderlinedLink from "../../components/UnderlinedLink";
 import Welcome from "../../components/Welcome";
-import FullStackProjects from "./FullStack";
 
 /**
  * Project Experience Component
@@ -36,9 +35,58 @@ function ProjectExperience() {
 				))}
 			</Welcome>
 			<div className="content-container-no-top">
-				<SlideIntoViewAnimation>
-					<FullStackProjects></FullStackProjects>
-				</SlideIntoViewAnimation>
+				<UppercasedText
+					text={"About"}
+					bold={true}
+					classes={"text-3xl"}
+				></UppercasedText>
+				<p className="mt-4">
+					Hi! Here's a project showcase of all of my open source
+					projects over the past five years! I divided them up into
+					the following categories to make navigation easier:
+				</p>
+				<ul>
+					<li>
+						<UnderlinedLink href="/projects/applied-ml">
+							Applied ML
+						</UnderlinedLink>
+					</li>
+					<li>
+						<UnderlinedLink href="/projects/web-dev">
+							Web Development
+						</UnderlinedLink>
+					</li>
+					<li>
+						<UnderlinedLink href="/projects/kaggle">
+							Kaggle
+						</UnderlinedLink>
+					</li>
+				</ul>
+				<p>
+					You can find my GitHub profile{" "}
+					<UnderlinedLink href="https://github.com/jchen42703">
+						here
+					</UnderlinedLink>
+					. Currently, I'm working on contributing to{" "}
+					<UnderlinedLink href="https://github.com/cockroachdb/cockroach/">
+						CockroachDB
+					</UnderlinedLink>{" "}
+					and{" "}
+					<UnderlinedLink href="https://github.com/ory/">
+						Ory
+					</UnderlinedLink>
+					. I'm also building a{" "}
+					<UnderlinedLink href="https://github.com/jchen42703/create-fullstack">
+						CLI
+					</UnderlinedLink>{" "}
+					to make generating production-grade code starter templates
+					for frontend, backend, and Docker Compose/Kubernetes
+					deployments easier and building a fullstack web app for
+					making novel translation easier called{" "}
+					<UnderlinedLink href="https://github.com/EasierMTL">
+						EasierMTL.
+					</UnderlinedLink>
+				</p>
 			</div>
 		</div>
 	);
